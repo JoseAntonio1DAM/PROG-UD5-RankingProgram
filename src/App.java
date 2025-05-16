34903263c194d56aa1f9a2f566351a5d9aaf9f5b
José Antonio Herrera Expósito <usr4034@salesianos-lacuesta.net>
2025-05-16 12:38:56 +0100

    ponemos a READ.ME y .gitignore en 'docs'

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        JOptionPane jp = new JOptionPane();
     }
 }
---
+++ src/net/salesianos/futbolistas/Elemento.java
@@ -0,0 +1,5 @@
+package net.salesianos.futbolistas;
+
+public class Elemento {
+
+}
